a8d398bb3f77484d74baeba2f899f304b064ffd7
Alex_Dawson <alex@passacaglia.design>
2019-03-17 12:49:59 +1100

    trying to return filtered list to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import BookList from './BookList';
 import BookFilter from './BookFilter';
-// import axios from 'axios';
-// import GoogleBooks from '../api/GoogleBooks';
+import BookList from './BookList';
 
 class App extends React.Component {
-	state = { selectedFilter: 'All', FilteredBooks: [] };
+	state = { selectedFilter: 'All', FilteredBookList: [] };
 
-	// onApplyFilter = async (Filter) => {
-	// 	//Receive array of filtered books
-	// 	//loop through array items and fetch book title
-	// 	//make api call with book titles (one multiple api call should be quicker than 5+ individual calls)
-	// 	const response = await GoogleBooks.get('/books/v1/volumes/', {
-	// 		params: {
-	// 			q: title
-	// 		}
-	// 	});
-	// };
+	createFilteredBookList = (filteredBooks) => {
+		this.setState({ FilteredBookList: filteredBooks });
+	};
 
 	onSelectFilter = (Filter) => {
+		console.log(Filter);
 		this.setState({ selectedFilter: Filter });
 	};
 
 	render() {
 		return (
 			<div>
-				<BookFilter onSelectFilter={this.onSelectFilter} selectedFilter={this.state.selectedFilter} />
-				<BookList selectedFilter={this.state.selectedFilter} />
+				<BookFilter onSelectFilter={this.onSelectFilter} />
+				<BookList
+					createFilteredBookList={this.createFilteredBookList}
+					selectedFilter={this.state.selectedFilter}
+				/>
 			</div>
 		);
 	}
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,54 +1,17 @@
 import React from 'react';
-import BookItem from './BookItem';
-import styled from '@emotion/styled';
 
-const BookShelf = styled.div`
-	/* Flex */
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-between;
+const books = [
+	{ title: 'Human Errors', status: 'Completed' },
+	{ title: 'Radical Technologies: The Design of Everyday Life', status: 'In progress' },
+	{ title: 'The Death and Life of Great American Cities', status: 'In progress' },
+	{ title: 'Primate Change', status: 'Completed' },
+	{ title: 'Usable Usability', status: 'Queued' },
+	{ title: 'Thinking, Fast and Slow', status: 'Completed' },
+	{ title: 'The Design of Everyday Objects', status: 'In progress' }
+];
 
-	/* Container */
-	max-width: 900px;
-	margin: 1rem auto 0 auto;
-
-	/* Media Queries */
-	@media (max-width: 700px) {
-		flex-direction: column;
-		flex: 1;
-	}
-`;
-
-class BookList extends React.Component {
-	state = {
-		books: [
-			{ title: 'Human Errors', status: 'Completed' },
-			{ title: 'Radical Technologies: The Design of Everyday Life', status: 'In progress' },
-			{ title: 'The Death and Life of Great American Cities', status: 'In progress' },
-			{ title: 'Primate Change', status: 'Completed' },
-			{ title: 'Usable Usability', status: 'Queued' },
-			{ title: 'Thinking, Fast and Slow', status: 'Completed' },
-			{ title: 'The Design of Everyday Objects', status: 'In progress' }
-		],
-
-		filteredBooks: []
-	};
-
-	createBookItem = (book, index) => {
-		return <BookItem title={book.title} key={index} />;
-	};
-
-	checkBookStatus = (book, index) => {
-		if (book.status === this.props.selectedFilter) {
-			return book.title;
-		} else if (this.props.selectedFilter === 'All') {
-			return this.state.books[index].title;
-		}
-	};
-
-	render() {
-		return <BookShelf>{this.state.books.filter(this.checkBookStatus).map(this.createBookItem)}</BookShelf>;
-	}
-}
+const BookList = (props) => {
+	return "Oee look at me I'm the BookList";
+};
 
 export default BookList;
